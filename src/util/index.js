9b7c2aab3f14bb4c854f3aafb50f66d9fdea9541
Kevin Eslick <eslick.kevin@yahoo.com>
2021-01-08 22:54:58 -0600

    removed some images

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,49 @@
 import React from 'react'
+import one from '../imgs/1.svg'
+import two from '../imgs/2.svg'
+import three from '../imgs/3.svg'
+import four from '../imgs/4.svg'
+import five from '../imgs/5.svg'
+import six from '../imgs/6.svg'
+import seven from '../imgs/7.svg'
+import eight from '../imgs/8.svg'
+import nine from '../imgs/9.svg'
+import ten from '../imgs/10.svg'
+import eleven from '../imgs/11.svg'
+import twelve from '../imgs/12.svg'
 
-function randomize() {
-	return findPoke
-}
+const cardsArr = [
+	{
+		id: 'take-five',
+		image: imageTakeFive,
+	},
+	{
+		id: 'garner-sea',
+		image: garnerSea,
+	},
+	{
+		id: 'peter-gunn',
+		image: peterGunn,
+	},
+	{
+		id: 'my-ship',
+		image: myShip,
+	},
+	{
+		id: 'shearing-wilson',
+		image: shearingWilson,
+	},
+	{
+		id: 'kind-of-blue',
+		image: kindOfBlue,
+	},
+	{
+		id: 'evans-portrait',
+		image: portraitInJazz,
+	},
+	{
+		id: 'peterson-porter',
+		image: petersonPorter,
+	},
+]
+function randomize() {}
