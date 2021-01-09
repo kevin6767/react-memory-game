d3995edaa95f055e3cf4d0ec6a9bf46613ddbfe4
Kevin Eslick <eslick.kevin@yahoo.com>
2021-01-08 23:38:52 -0600

    adding github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import './App.css'
+import React, { useState } from 'react'
+
+import Board from './components/Board'
+import { cardCreation } from './util'
+
 function App() {
-	return <div className='App'>hello!</div>
+	const [cardsArr, updateCardsArr] = useState(cardCreation())
+
+	return (
+		<div className='wrapper'>
+			<div className='game-container'>
+				<h1>Hellow!</h1>
+				<Board cardsArr={cardsArr} />
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/util/index.js
+++ src/util/index.js
@@ -1,49 +1,80 @@
 import React from 'react'
-import one from '../imgs/1.svg'
-import two from '../imgs/2.svg'
-import three from '../imgs/3.svg'
-import four from '../imgs/4.svg'
-import five from '../imgs/5.svg'
-import six from '../imgs/6.svg'
-import seven from '../imgs/7.svg'
-import eight from '../imgs/8.svg'
-import nine from '../imgs/9.svg'
-import ten from '../imgs/10.svg'
-import eleven from '../imgs/11.svg'
-import twelve from '../imgs/12.svg'
+import Bulbasaur from '../imgs/1.svg'
+import Ivysaur from '../imgs/2.svg'
+import Venusaur from '../imgs/3.svg'
+import Charmander from '../imgs/4.svg'
+import Charmeleon from '../imgs/5.svg'
+import Charizard from '../imgs/6.svg'
+import Squirtle from '../imgs/7.svg'
+import Wartortle from '../imgs/8.svg'
+import Blastoise from '../imgs/9.svg'
+import Caterpie from '../imgs/10.svg'
+import Metapod from '../imgs/11.svg'
+import Butterfree from '../imgs/12.svg'
 
-const cardsArr = [
+const pokeArray = [
 	{
-		id: 'take-five',
-		image: imageTakeFive,
+		id: 1,
+		image: Bulbasaur,
 	},
 	{
-		id: 'garner-sea',
-		image: garnerSea,
+		id: 2,
+		image: Ivysaur,
 	},
 	{
-		id: 'peter-gunn',
-		image: peterGunn,
+		id: 3,
+		image: Venusaur,
 	},
 	{
-		id: 'my-ship',
-		image: myShip,
+		id: 4,
+		image: Charmander,
 	},
 	{
-		id: 'shearing-wilson',
-		image: shearingWilson,
+		id: 5,
+		image: Charmeleon,
 	},
 	{
-		id: 'kind-of-blue',
-		image: kindOfBlue,
+		id: 6,
+		image: Charizard,
 	},
 	{
-		id: 'evans-portrait',
-		image: portraitInJazz,
+		id: 7,
+		image: Squirtle,
 	},
 	{
-		id: 'peterson-porter',
-		image: petersonPorter,
+		id: 8,
+		image: Wartortle,
+	},
+	{
+		id: 9,
+		image: Blastoise,
+	},
+	{
+		id: 10,
+		image: Caterpie,
+	},
+	{
+		id: 11,
+		image: Metapod,
+	},
+	{
+		id: 12,
+		image: Butterfree,
 	},
 ]
-function randomize() {}
+function randomize(array) {
+	let i = array.length - 1
+	for (; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		const temp = array[i]
+		array[i] = array[j]
+		array[j] = temp
+	}
+	return array
+}
+
+function cardCreation() {
+	return randomize(pokeArray.concat(pokeArray))
+}
+
+export { cardCreation }
